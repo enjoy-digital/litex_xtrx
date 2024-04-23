1bd500af521004e9d7529576251674cae5032675
Gwenhael Goavec-Merou <gwenhael@enjoy-digital.fr>
2024-04-23 13:44:45 +0200

    software/lms7002mNG: importe source from LimeSuiteNG/src/lms7002m

---
+++ software/lms7002mNG/src/LMS7002M_validation.h
@@ -0,0 +1,11 @@
+#ifndef LMS7_VALIDATION
+#define LMS7_VALIDATION
+
+namespace lime {
+
+// Checks if config parameters are within expected values and can produce working state
+bool LMS7002M_Validate(const SDRConfig& config, std::vector<std::string>& out_errors, const int lms7002_chCount = 2);
+
+} // namespace lime
+
+#endif // LMS7_VALIDATION
