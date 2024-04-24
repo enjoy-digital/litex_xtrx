a3791f9f21548b98603b207a187dd2ebd9269421
Gwenhael Goavec-Merou <gwenhael@enjoy-digital.fr>
2024-04-24 18:34:17 +0200

    software/soapysdr-xtrx/XTRXDevice.cpp: partial ctor -> LimeSuiteNG

--- software/soapysdr-xtrx/XTRXDevice.cpp
+++ software/soapysdr-xtrx/XTRXDevice.cpp
@@ -30,13 +30,14 @@
 
 #include <lms7002mNG/IComms.h>
 #include <lms7002mNG/OpStatus.h>
+#include <lms7002mNG/LMS7002M_parameters.h>
 
 #define USE_NG
 //#undef USE_NG
 
 class DLL_EXPORT LMS_SPI: public lime::ISPI {
     public:
-        LMS_SPI(LMS7002M_t *lms, int fd):_lms(lms), _fd(fd) {}
+        LMS_SPI(int fd):_fd(fd) {}
 
         lime::OpStatus SPI(const uint32_t* MOSI, uint32_t* MISO, uint32_t count) override
         {
@@ -68,7 +69,6 @@ class DLL_EXPORT LMS_SPI: public lime::ISPI {
         }
 
     private:
-        LMS7002M_t *_lms;
         int _fd;
 };
 
@@ -160,7 +160,7 @@ SoapyLiteXXTRX::SoapyLiteXXTRX(const SoapySDR::Kwargs &args)
     }
     /* hack */
 #ifdef USE_NG
-    _lms_spi = std::make_shared<LMS_SPI>(_lms, _fd);
+    _lms_spi = std::make_shared<LMS_SPI>(_fd);
     _lms2 = new lime::LMS7002M(_lms_spi);
     _lms2->SetReferenceClk_SX(lime::TRXDir::Rx, _refClockRate);
     _lms2->SetClockFreq(lime::LMS7002M::ClockID::CLK_REFERENCE, _refClockRate);
@@ -171,10 +171,15 @@ SoapyLiteXXTRX::SoapyLiteXXTRX(const SoapySDR::Kwargs &args)
 #endif
 
     // read info register
+#ifdef USE_NG
+	uint16_t ver = _lms2->Get_SPI_Reg_bits(LMS7param(VER));
+	uint16_t rev = _lms2->Get_SPI_Reg_bits(LMS7param(REV));
+#else
     LMS7002M_regs_spi_read(_lms, 0x002f);
-    SoapySDR::logf(SOAPY_SDR_INFO, "LMS7002M info: revision %d, version %d",
-                   LMS7002M_regs(_lms)->reg_0x002f_rev,
-                   LMS7002M_regs(_lms)->reg_0x002f_ver);
+	uint16_t ver = LMS7002M_regs(_lms)->reg_0x002f_ver;
+	uint16_t rev = LMS7002M_regs(_lms)->reg_0x002f_rev;
+#endif
+    SoapySDR::logf(SOAPY_SDR_INFO, "LMS7002M info: revision %d, version %d", rev, ver);
 
     // set clock to Internal Reference Clock
     this->setClockSource("internal");
@@ -184,6 +189,12 @@ SoapyLiteXXTRX::SoapyLiteXXTRX(const SoapySDR::Kwargs &args)
     LMS7002M_configure_lml_port(_lms, LMS_PORT1, LMS_RX, 1);
 
     // enable components
+#ifdef USE_NG
+	_lms2->EnableChannel(lime::TRXDir::Tx, 0, false);  // LMS_CHA
+	_lms2->EnableChannel(lime::TRXDir::Tx, 1, false);  // LMS_CHB
+	_lms2->EnableChannel(lime::TRXDir::Rx, 0, false);  // LMS_CHA
+	_lms2->EnableChannel(lime::TRXDir::Rx, 1, false);  // LMS_CHB
+#else
     LMS7002M_afe_enable(_lms, LMS_TX, LMS_CHA, true);
     LMS7002M_afe_enable(_lms, LMS_TX, LMS_CHB, true);
     LMS7002M_afe_enable(_lms, LMS_RX, LMS_CHA, true);
@@ -202,6 +213,7 @@ SoapyLiteXXTRX::SoapyLiteXXTRX(const SoapySDR::Kwargs &args)
     LMS7002M_trf_enable(_lms, LMS_CHB, true);
     LMS7002M_sxx_enable(_lms, LMS_RX, true);
     LMS7002M_sxx_enable(_lms, LMS_TX, true);
+#endif
 
     // XTRX-specific configuration
     LMS7002M_ldo_enable(_lms, true, LMS7002M_LDO_ALL);
@@ -267,6 +279,95 @@ SoapyLiteXXTRX::SoapyLiteXXTRX(const SoapySDR::Kwargs &args)
 
     SoapySDR::log(SOAPY_SDR_INFO, "SoapyLiteXXTRX initialization complete");
 }
+
+#if 0
+SoapyLiteXXTRX::configure_lml_port(uint8_t portNo, direction, int mckDiv)
+{
+#ifdef USE_NG
+	    //LML is in global register space
+    LMS7002M_set_mac_ch(self, LMS_CHAB);
+
+    //set TRXIQ on both ports
+    if (portNo == LMS_PORT1)
+    {
+        self->regs->reg_0x0023_lml1_mode = REG_0X0023_LML1_MODE_TRXIQ;
+        self->regs->reg_0x0023_lml1_rxntxiq = (direction==LMS_TX)?
+            REG_0X0023_LML1_RXNTXIQ_RXIQ:REG_0X0023_LML1_RXNTXIQ_TXIQ; //WARNING: TX/RX perspective swap
+    }
+    if (portNo == LMS_PORT2)
+    {
+        self->regs->reg_0x0023_lml2_mode = REG_0X0023_LML2_MODE_TRXIQ;
+        self->regs->reg_0x0023_lml2_rxntxiq = (direction==LMS_TX)?
+            REG_0X0023_LML2_RXNTXIQ_RXIQ:REG_0X0023_LML2_RXNTXIQ_TXIQ; //WARNING: TX/RX perspective swap
+    }
+
+    //automatic directions based on mode above
+    self->regs->reg_0x0023_enabledirctr1 = 0;
+    self->regs->reg_0x0023_enabledirctr2 = 0;
+
+    //set the FIFO rd and wr clock muxes based on direction
+    if (direction == LMS_TX)
+    {
+        self->regs->reg_0x002a_txrdclk_mux = REG_0X002A_TXRDCLK_MUX_TXTSPCLK;
+        self->regs->reg_0x002a_txwrclk_mux = (portNo==LMS_PORT1)?REG_0X002A_TXWRCLK_MUX_FCLK1:REG_0X002A_TXWRCLK_MUX_FCLK2;
+    }
+    if (direction == LMS_RX)
+    {
+        self->regs->reg_0x002a_rxrdclk_mux = (portNo==LMS_PORT1)?REG_0X002A_RXRDCLK_MUX_MCLK1:REG_0X002A_RXRDCLK_MUX_MCLK2;
+        self->regs->reg_0x002a_rxwrclk_mux = REG_0X002A_RXWRCLK_MUX_RXTSPCLK;
+    }
+
+    //data stream muxes
+    if (direction == LMS_TX)
+    {
+        self->regs->reg_0x002a_tx_mux = (portNo==LMS_PORT1)?REG_0X002A_TX_MUX_PORT1:REG_0X002A_TX_MUX_PORT2;
+    }
+    if (direction == LMS_RX)
+    {
+        self->regs->reg_0x002a_rx_mux = REG_0X002A_RX_MUX_RXTSP;
+    }
+
+    //clock mux (outputs to mclk pin)
+    if (portNo == LMS_PORT1)
+    {
+        self->regs->reg_0x002b_mclk1src = (direction==LMS_TX)?
+            ((mclkDiv==1)?REG_0X002B_MCLK1SRC_TXTSPCLKA:REG_0X002B_MCLK1SRC_TXTSPCLKA_DIV):
+            ((mclkDiv==1)?REG_0X002B_MCLK1SRC_RXTSPCLKA:REG_0X002B_MCLK1SRC_RXTSPCLKA_DIV);
+    }
+    if (portNo == LMS_PORT2)
+    {
+        self->regs->reg_0x002b_mclk2src = (direction==LMS_TX)?
+            ((mclkDiv==1)?REG_0X002B_MCLK2SRC_TXTSPCLKA:REG_0X002B_MCLK2SRC_TXTSPCLKA_DIV):
+            ((mclkDiv==1)?REG_0X002B_MCLK2SRC_RXTSPCLKA:REG_0X002B_MCLK2SRC_RXTSPCLKA_DIV);
+    }
+
+	//clock divider (outputs to mclk pin)
+    if (direction == LMS_TX)
+    {
+        self->regs->reg_0x002b_txdiven = (mclkDiv > 1)?1:0;
+        self->regs->reg_0x002c_txtspclk_div = (mclkDiv/2)-1;
+    }
+    if (direction == LMS_RX)
+    {
+        self->regs->reg_0x002b_rxdiven = (mclkDiv > 1)?1:0;
+        self->regs->reg_0x002c_rxtspclk_div = (mclkDiv/2)-1;
+    }
+
+    // TODO: Elliot has found that we need to invert the LML clocks
+    //       to avoid terrible spiking behavior when running a
+    //       TBB baseband loopback.
+    self->regs->reg_0x00ad_value = 0x03f3;
+
+    LMS7002M_regs_spi_write(self, 0x0023);
+    LMS7002M_regs_spi_write(self, 0x002A);
+    LMS7002M_regs_spi_write(self, 0x002B);
+    LMS7002M_regs_spi_write(self, 0x002C);
+    LMS7002M_regs_spi_write(self, 0x00AD);
+#else
+    LMS7002M_configure_lml_port(_lms, LMS_PORT2, LMS_TX, 1);
+#endif
+}
+#endif
 
 SoapyLiteXXTRX::~SoapyLiteXXTRX(void) {
     SoapySDR::log(SOAPY_SDR_INFO, "Power down and cleanup");
