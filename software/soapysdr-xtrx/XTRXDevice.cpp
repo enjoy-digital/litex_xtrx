24b2a98866bdafc0bf12da4a9dd150fd6df49acb
Florent Kermarrec <florent@enjoy-digital.fr>
2024-05-06 17:40:30 +0200

    software/soapysdr-xtrx: Avoied litepcie_interface.h file and directly integrate it in XTRXDevice.cpp.

--- software/soapysdr-xtrx/XTRXDevice.cpp
+++ software/soapysdr-xtrx/XTRXDevice.cpp
@@ -25,7 +25,6 @@
 //#define USE_OLD
 
 #include "XTRXDevice.hpp"
-#include "litepcie_interface.h"
 #include <SoapySDR/Registry.hpp>
 #include <SoapySDR/Logger.hpp>
 #include <fstream>
@@ -116,6 +115,51 @@ const std::vector<std::pair<uint16_t, uint16_t>> xtrx_default_cfg = {
                       // [3] EN_TBUFIN_XBUF_RX: RX XBUF input is coming from TX
 };
 
+#define LITEPCIE_SPI_CS_HIGH (0 << 0)
+#define LITEPCIE_SPI_CS_LOW  (1 << 0)
+#define LITEPCIE_SPI_START   (1 << 0)
+#define LITEPCIE_SPI_DONE    (1 << 0)
+#define LITEPCIE_SPI_LENGTH  (1 << 8)
+
+extern "C" {
+
+#include "liblitepcie.h"
+
+#include <fcntl.h>
+#include <unistd.h>
+
+//#define DBG_TRANSACTION
+
+static inline uint32_t litepcie_interface_transact(void *handle, const uint32_t data_in, const bool readback)
+{
+    int *fd = (int *)handle;
+
+    //load tx data
+    litepcie_writel(*fd, CSR_LMS7002M_SPI_MOSI_ADDR, data_in);
+
+    //start transaction
+    litepcie_writel(*fd, CSR_LMS7002M_SPI_CONTROL_ADDR, 32*LITEPCIE_SPI_LENGTH | LITEPCIE_SPI_START);
+
+    //wait for completion
+    while ((litepcie_readl(*fd, CSR_LMS7002M_SPI_STATUS_ADDR) & LITEPCIE_SPI_DONE) == 0);
+
+    //load rx data
+    if (readback) {
+        uint32_t ret = litepcie_readl(*fd, CSR_LMS7002M_SPI_MISO_ADDR) & 0xffff;
+#ifdef DBG_TRANSACTION
+        printf("%s read  addr: 0x%04x -> %08x\n", __func__, 0x7fff & (data_in >> 16), ret);
+#endif
+        return ret;
+    } else {
+#ifdef DBG_TRANSACTION
+        printf("%s write addr: 0x%04x value: 0x%04x\n", __func__,
+            0x7fff & (data_in >> 16), data_in & 0xffff);
+#endif
+        return 0;
+    }
+}
+
+}
 
 class DLL_EXPORT LMS_SPI: public lime::ISPI {
     public:
