76f76283ea2a30ab7aee5438493edfad128fa690
Florent Kermarrec <florent@enjoy-digital.fr>
2024-05-07 22:14:08 +0200

    software: Rename soapy_sdr to soapysdr.

--- software/build_driver.py
+++ software/build_driver.py
@@ -24,4 +24,4 @@ def build_driver(path, cmake_options=""):
 run_command("cd kernel && make clean all")
 run_command("cd user   && make clean all")
 build_driver("lms7002m")
-build_driver("soapy_sdr", "-DCMAKE_INSTALL_PREFIX=/usr")
+build_driver("soapysdr", "-DCMAKE_INSTALL_PREFIX=/usr")
